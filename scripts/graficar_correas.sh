6e5e19fa72240dc1d4184bcfe40baf1a8f487bed
valdidomo <valdidomo@gmail.com>
2026-06-16 22:13:58 +0200

    Copia de seguridad automática: 2026-06-16 22:13:58

--- scripts/graficar_correas.sh
+++ scripts/graficar_correas.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
-# Script Maestro Input Shaper y Correas para BLV MGN Cube
-
-# Esperar a que Klipper termine de escribir en el disco
-sleep 5
-
-# Crear la carpeta destino si no existe
-mkdir -p /home/pi/printer_data/config/input_shaper
-
-# 1. COMPROBAR SI ES UN TEST DE CORREAS (Archivos raw_data_axis)
-if ls /tmp/raw_data_axis_*.csv 1> /dev/null 2>&1; then
-    echo "Procesando gráfico de tensión de correas..."
-    python3 ~/klipper/scripts/graph_accelerometer.py /tmp/raw_data_axis_*.csv -o /home/pi/printer_data/config/input_shaper/tension_correas.png
-    rm -f /tmp/raw_data_axis_*.csv
+NEW_DIR="/home/pi/printer_data/config/res_helper"
+if [ ! -d "$NEW_DIR" ]; then
+    mkdir -p "$NEW_DIR"
 fi
+~/klipper/scripts/calibrate_shaper.py /tmp/raw_data_axis=1.000,1.000,0.000_*.csv -o "${NEW_DIR}/belt_tension_x.png"
+~/klipper/scripts/calibrate_shaper.py /tmp/raw_data_axis=1.000,-1.000,0.000_*.csv -o "${NEW_DIR}/belt_tension_y.png"
 
-# 2. COMPROBAR SI ES EL EJE X (Archivos calibration_data_x)
-if ls /tmp/calibration_data_x_*.csv 1> /dev/null 2>&1; then
-    echo "Procesando gráfico del Eje X..."
-    python3 ~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o /home/pi/printer_data/config/input_shaper/resonancias_x.png
-    rm -f /tmp/calibration_data_x_*.csv
-fi
-
-# 3. COMPROBAR SI ES EL EJE Y (Archivos calibration_data_y)
-if ls /tmp/calibration_data_y_*.csv 1> /dev/null 2>&1; then
-    echo "Procesando gráfico del Eje Y..."
-    python3 ~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o /home/pi/printer_data/config/input_shaper/resonancias_y.png
-    rm -f /tmp/calibration_data_y_*.csv
-fi
-
-echo "Procesamiento completado con éxito."
 
