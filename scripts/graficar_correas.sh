74c43e6c39d2eecc96ae86a447285e74ff53a8f0
valdidomo <valdidomo@gmail.com>
2026-06-17 11:02:05 +0200

    Copia de seguridad automática: 2026-06-17 11:02:05

--- scripts/graficar_correas.sh
+++ scripts/graficar_correas.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-NEW_DIR="/home/pi/printer_data/config/res_helper"
+NEW_DIR="/home/pi/printer_data/config/input_shaper"
 if [ ! -d "$NEW_DIR" ]; then
     mkdir -p "$NEW_DIR"
 fi
-~/klipper/scripts/calibrate_shaper.py /tmp/raw_data_axis=1.000,1.000,0.000_*.csv -o "${NEW_DIR}/belt_tension_x.png"
-~/klipper/scripts/calibrate_shaper.py /tmp/raw_data_axis=1.000,-1.000,0.000_*.csv -o "${NEW_DIR}/belt_tension_y.png"
 
+# Procesa las imágenes de correas si existen los datos temporales
+~/klipper/scripts/calibrate_shaper.py /tmp/raw_data_axis=1.000,1.000,0.000_*.csv -o "${NEW_DIR}/belt_tension_x.png" 2>/dev/null
+~/klipper/scripts/calibrate_shaper.py /tmp/raw_data_axis=1.000,-1.000,0.000_*.csv -o "${NEW_DIR}/belt_tension_y.png" 2>/dev/null
 
+# Procesa las imágenes de resonancia de los ejes X e Y si existen los datos
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o "${NEW_DIR}/resonancias_x.png" 2>/dev/null
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o "${NEW_DIR}/resonancias_y.png" 2>/dev/null
