84ebae6ba7b5a3802a3286245c16d68ae1db0814
valdidomo <valdidomo@gmail.com>
2026-06-17 12:18:31 +0200

    Copia de seguridad automática: 2026-06-17 12:18:31

--- scripts/graficar_correas.sh
+++ scripts/graficar_correas.sh
@@ -7,6 +7,6 @@ fi
 # 1. Gráfica comparativa de tensión de Correas (Datos en diagonal)
 ~/klipper/scripts/calibrate_shaper.py /tmp/raw_data_axis=*.csv -o "${NEW_DIR}/belt_tension_comparativa.png" 2>/dev/null
 
-# 2. Gráficas individuales de Resonancias X e Y (Datos puros de ejes)
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o "${NEW_DIR}/resonancias_x.png" 2>/dev/null
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o "${NEW_DIR}/resonancias_y.png" 2>/dev/null
+# 2. Gráficas individuales de Resonancias X e Y (Datos puros de ejes de Klipper)
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o "${NEW_DIR}/resonancias_x.png" 2>/dev/null
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o "${NEW_DIR}/resonancias_y.png" 2>/dev/null
