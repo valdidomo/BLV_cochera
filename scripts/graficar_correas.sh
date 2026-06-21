e36ff86986b107c3ffa73db97118abade393ffb2
valdidomo <valdidomo@gmail.com>
2026-06-21 16:51:10 +0200

    Copia de seguridad automática: 2026-06-21 16:51:10

--- scripts/graficar_correas.sh
+++ scripts/graficar_correas.sh
@@ -7,27 +7,25 @@ fi
 
 case "$1" in
     clean)
-        # Limpieza de archivos temporales viejos para evitar solapamientos
+        # Vacía archivos anteriores antes de empezar para que la gráfica salga con una sola curva
         rm -f "$NEW_DIR"/*.png
         rm -f /tmp/raw_data_axis*.csv
         rm -f /tmp/resonances_*.csv
         rm -f /tmp/calibration_data_*.csv
         ;;
     belts)
-        # Gráfica de Correas (Corregido: Busca tanto 'raw_data_axis' como 'resonances_axis' para asegurar que lea tus archivos)
         /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/graph_accelerometer.py /tmp/*axis*.csv -o "${NEW_DIR}/belt_tension_comparativa.png" 2>/dev/null
         rm -f /tmp/*axis*.csv
         ;;
     shaper_x)
-        # Input Shaper Eje X (Limpia y procesa los datos específicos del eje X)
         /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o "${NEW_DIR}/resonancias_x.png" 2>/dev/null
         rm -f /tmp/calibration_data_x_*.csv
         ;;
     shaper_y)
-        # Input Shaper Eje Y (Limpia y procesa los datos específicos del eje Y)
         /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o "${NEW_DIR}/resonancias_y.png" 2>/dev/null
         rm -f /tmp/calibration_data_y_*.csv
         ;;
 esac
+
 
 
