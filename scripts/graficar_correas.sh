b09eb4dcfdbbc79a57ad54820aa46c75a8416425
valdidomo <valdidomo@gmail.com>
2026-06-21 16:41:13 +0200

    Copia de seguridad automática: 2026-06-21 16:41:13

--- scripts/graficar_correas.sh
+++ scripts/graficar_correas.sh
@@ -7,22 +7,26 @@ fi
 
 case "$1" in
     clean)
+        # Limpieza de archivos temporales viejos para evitar solapamientos
         rm -f "$NEW_DIR"/*.png
         rm -f /tmp/raw_data_axis*.csv
         rm -f /tmp/resonances_*.csv
         rm -f /tmp/calibration_data_*.csv
         ;;
     belts)
-        # Gráfica de Correas (Rutas absolutas fijas sin el símbolo ~)
-        /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/graph_accelerometer.py /tmp/raw_data_axis*.csv -o "${NEW_DIR}/belt_tension_comparativa.png" 2>/dev/null
+        # Gráfica de Correas (Corregido: Busca tanto 'raw_data_axis' como 'resonances_axis' para asegurar que lea tus archivos)
+        /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/graph_accelerometer.py /tmp/*axis*.csv -o "${NEW_DIR}/belt_tension_comparativa.png" 2>/dev/null
+        rm -f /tmp/*axis*.csv
         ;;
     shaper_x)
-        # Input Shaper X (Rutas absolutas fijas sin el símbolo ~)
+        # Input Shaper Eje X (Limpia y procesa los datos específicos del eje X)
         /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o "${NEW_DIR}/resonancias_x.png" 2>/dev/null
+        rm -f /tmp/calibration_data_x_*.csv
         ;;
     shaper_y)
-        # Input Shaper Y (Rutas absolutas fijas sin el símbolo ~)
+        # Input Shaper Eje Y (Limpia y procesa los datos específicos del eje Y)
         /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o "${NEW_DIR}/resonancias_y.png" 2>/dev/null
+        rm -f /tmp/calibration_data_y_*.csv
         ;;
 esac
 
