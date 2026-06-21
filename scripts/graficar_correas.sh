c8caa5d4db9b1a254650cf3bbe0cd1e8e7eb17eb
valdidomo <valdidomo@gmail.com>
2026-06-21 17:34:14 +0200

    Copia de seguridad automática: 2026-06-21 17:34:14

--- scripts/graficar_correas.sh
+++ scripts/graficar_correas.sh
@@ -7,25 +7,36 @@ fi
 
 case "$1" in
     clean)
-        # Vacía archivos anteriores antes de empezar para que la gráfica salga con una sola curva
-        rm -f "$NEW_DIR"/*.png
-        rm -f /tmp/raw_data_axis*.csv
-        rm -f /tmp/resonances_*.csv
-        rm -f /tmp/calibration_data_*.csv
+        # !!! CORREGIDO: Ya NO borramos los archivos .png de Mainsail !!!
+        # Solo desalojamos los archivos CSV viejos de la carpeta /tmp
+        mkdir -p /home/pi/printer_data/config/scripts/.tmp_clean
+        mv /tmp/calibration_data_*.csv /home/pi/printer_data/config/scripts/.tmp_clean/ 2>/dev/null
+        mv /tmp/resonances_*.csv /home/pi/printer_data/config/scripts/.tmp_clean/ 2>/dev/null
+        mv /tmp/raw_data_axis*.csv /home/pi/printer_data/config/scripts/.tmp_clean/ 2>/dev/null
+        rm -rf /home/pi/printer_data/config/scripts/.tmp_clean
         ;;
     belts)
+        # 1. Gráfica de Correas: resonances_axis o raw_data_axis
         /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/graph_accelerometer.py /tmp/*axis*.csv -o "${NEW_DIR}/belt_tension_comparativa.png" 2>/dev/null
-        rm -f /tmp/*axis*.csv
+        
+        mkdir -p /home/pi/printer_data/config/scripts/.tmp_clean
+        mv /tmp/*axis*.csv /home/pi/printer_data/config/scripts/.tmp_clean/ 2>/dev/null
+        rm -rf /home/pi/printer_data/config/scripts/.tmp_clean
         ;;
     shaper_x)
-        /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o "${NEW_DIR}/resonancias_x.png" 2>/dev/null
-        rm -f /tmp/calibration_data_x_*.csv
+        # 2. Gráfica de Input Shaper X: resonances_x o calibration_data_x
+        /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/*_x_*.csv -o "${NEW_DIR}/resonancias_x.png" 2>/dev/null
+        
+        mkdir -p /home/pi/printer_data/config/scripts/.tmp_clean
+        mv /tmp/*_x_*.csv /home/pi/printer_data/config/scripts/.tmp_clean/ 2>/dev/null
+        rm -rf /home/pi/printer_data/config/scripts/.tmp_clean
         ;;
     shaper_y)
-        /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o "${NEW_DIR}/resonancias_y.png" 2>/dev/null
-        rm -f /tmp/calibration_data_y_*.csv
+        # 3. Gráfica de Input Shaper Y: resonances_y o calibration_data_y
+        /home/pi/klippy-env/bin/python /home/pi/klipper/scripts/calibrate_shaper.py /tmp/*_y_*.csv -o "${NEW_DIR}/resonancias_y.png" 2>/dev/null
+        
+        mkdir -p /home/pi/printer_data/config/scripts/.tmp_clean
+        mv /tmp/*_y_*.csv /home/pi/printer_data/config/scripts/.tmp_clean/ 2>/dev/null
+        rm -rf /home/pi/printer_data/config/scripts/.tmp_clean
         ;;
 esac
-
-
-
